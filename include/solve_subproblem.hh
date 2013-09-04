97ea76587f98d8990d54425282aeff022bef63bc
traviscj (mbair) <traviscj@traviscj.com>
2013-09-04 18:29:07 -0500

    solve_subproblem convenience functions to grab hessian/jacobian in qpOASES objects

--- include/solve_subproblem.hh
+++ include/solve_subproblem.hh
@@ -11,8 +11,16 @@ public:
 	SolveQuadraticProgram(Nlp &nlp);
 	
 
-	iSQOStep operator()(iSQOSparseQuadraticSubproblem &subproblem);
+    // iSQOStep operator()(iSQOSparseQuadraticSubproblem &subproblem);
 	iSQOStep operator()(iSQOQuadraticSubproblem &subproblem);
+    
+    qpOASES::SymmetricMatrix *get_qpoases_hessian(iSQOQuadraticSubproblem &subproblem, std::shared_ptr<matrix_base_class> hessian);
+    qpOASES::SymmetricMatrix *get_qpoases_hessian(iSQOQuadraticSubproblem &subproblem, std::shared_ptr<dense_matrix> hessian);
+    qpOASES::SymmetricMatrix *get_qpoases_hessian(iSQOQuadraticSubproblem &subproblem, std::shared_ptr<sparse_matrix> hessian);
+    qpOASES::Matrix *get_qpoases_jacobian(iSQOQuadraticSubproblem &subproblem, std::shared_ptr<matrix_base_class> jacobian);
+    qpOASES::Matrix *get_qpoases_jacobian(iSQOQuadraticSubproblem &subproblem, std::shared_ptr<dense_matrix> jacobian);
+    qpOASES::Matrix *get_qpoases_jacobian(iSQOQuadraticSubproblem &subproblem, std::shared_ptr<sparse_matrix> jacobian);
+    
 private:
     void operator_setup();
     iSQOStep operator_finish(const iSQOQuadraticSubproblem &subproblem, qpOASES::returnValue ret);

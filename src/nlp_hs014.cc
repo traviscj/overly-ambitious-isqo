552e893410d0bd9acdf60f975febfd9b4a8e1e61
traviscj (mbair) <traviscj@traviscj.com>
2013-09-04 18:32:12 -0500

    code so that hs014 returns shared_ptr objects

--- src/nlp_hs014.cc
+++ src/nlp_hs014.cc
@@ -36,27 +36,27 @@ std::vector<double> Hs014::objective_gradient(const iSQOIterate &iterate) {
 	ret_gradient[1] = 2*(iterate.primal_values_[1] - 1);
 	return ret_gradient;
 }
-matrix Hs014::constraints_equality_jacobian(const iSQOIterate &iterate){
-	matrix ret_jacobian(iterate.num_dual_ieq_, iterate.num_primal_);
-	ret_jacobian.set(0,0, 1.0);
-	ret_jacobian.set(0,1,-2.0);
+std::shared_ptr<matrix_base_class> Hs014::constraints_equality_jacobian(const iSQOIterate &iterate){
+	std::shared_ptr<dense_matrix> ret_jacobian(new dense_matrix(iterate.num_dual_ieq_, iterate.num_primal_));
+	ret_jacobian->set(0,0, 1.0);
+	ret_jacobian->set(0,1,-2.0);
 	return ret_jacobian;
 }
-matrix Hs014::constraints_inequality_jacobian(const iSQOIterate &iterate){
-	matrix ret_jacobian(iterate.num_dual_ieq_, iterate.num_primal_);
-	ret_jacobian.set(0,0,0.5*iterate.primal_values_[0]);
-	ret_jacobian.set(0,1,2.0*iterate.primal_values_[1]);
+std::shared_ptr<matrix_base_class> Hs014::constraints_inequality_jacobian(const iSQOIterate &iterate){
+	std::shared_ptr<dense_matrix> ret_jacobian(new dense_matrix(iterate.num_dual_ieq_, iterate.num_primal_));
+	ret_jacobian->set(0,0,0.5*iterate.primal_values_[0]);
+	ret_jacobian->set(0,1,2.0*iterate.primal_values_[1]);
 	return ret_jacobian;
 }
 
-matrix Hs014::lagrangian_hessian(const iSQOIterate &iterate) {
-	matrix hess(iterate.num_primal_, iterate.num_primal_);
+std::shared_ptr<matrix_base_class> Hs014::lagrangian_hessian(const iSQOIterate &iterate) {
+	std::shared_ptr<dense_matrix> hess(new dense_matrix(iterate.num_primal_, iterate.num_primal_));
 	if (iterate.penalty_parameter_ > 0) {
-		hess.set(0,0,iterate.penalty_parameter_*2.0 + 0.5*iterate.dual_ieq_values_[0]); //   /iterate.penalty_parameter_
-		hess.set(1,1,iterate.penalty_parameter_*2.0 + 2.0*iterate.dual_ieq_values_[0]);	//	/iterate.penalty_parameter_
+		hess->set(0,0,iterate.penalty_parameter_*2.0 + 0.5*iterate.dual_ieq_values_[0]); //   /iterate.penalty_parameter_
+		hess->set(1,1,iterate.penalty_parameter_*2.0 + 2.0*iterate.dual_ieq_values_[0]);	//	/iterate.penalty_parameter_
 	} else {
-		hess.set(0,0,0.5*iterate.dual_ieq_values_[0]);
-		hess.set(1,1,2.0*iterate.dual_ieq_values_[1]);
+		hess->set(0,0,0.5*iterate.dual_ieq_values_[0]);
+		hess->set(1,1,2.0*iterate.dual_ieq_values_[1]);
 	}
 	return hess;
 }

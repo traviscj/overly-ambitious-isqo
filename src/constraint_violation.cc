a17a3ea1a672668bb45bdf33e5ab3c3238fde3c9
traviscj (mbair) <traviscj@traviscj.com>
2013-09-10 12:03:18 -0500

    use dot product in constraint_violation

--- src/constraint_violation.cc
+++ src/constraint_violation.cc
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 #include "utilities.hh"
 #include "constraint_violation.hh"
@@ -21,23 +22,23 @@ double ConstraintViolationFunction::operator()(const iSQOIterate &iterate, const
 	std::vector<double> con_values_eq = nlp_->constraints_equality(iterate);
 	std::vector<double> con_values_ieq = nlp_->constraints_inequality(iterate);
 
-	if (PRINT) std::cout << "constraintviolationfunction e 0: " << con_values_eq << std::endl;
 	// J(x)*d
+    // if (PRINT) std::cout << "jac_ce: " << jac_ce << std::endl;
+    // if (PRINT) std::cout << "step: " << step.get_primal_values() << std::endl;
+	if (PRINT) std::cout << "constraintviolationfunction e 0: " << con_values_eq << std::endl;
 	std::shared_ptr<matrix_base_class> jac_ce = nlp_->constraints_equality_jacobian(iterate);
-	if (PRINT) std::cout << jac_ce;
-    std::vector<double> con_values_eq_mult_result = jac_ce->multiply(step.primal_values_);
-    for (size_t row=0; row<nlp_->num_dual_eq(); ++row) {
+    std::vector<double> con_values_eq_mult_result = jac_ce->multiply(step.get_primal_values());
+    for (size_t row=0; row < nlp_->num_dual_eq(); ++row) {
         con_values_eq[row] += con_values_eq_mult_result[row];
     }
 	if (PRINT) std::cout << "constraintviolationfunction e 1: " << con_values_eq << std::endl;
 	
 	// \bar{J}(x)*d
+    // if (PRINT) std::cout << "jac_ci: " << jac_ci << std::endl;
+    // if (PRINT) std::cout << "step: " << step.get_primal_values() << std::endl;
 	if (PRINT) std::cout << "constraintviolationfunction i 0: " << con_values_ieq << std::endl;
-    if (PRINT) std::cout << "step: " << step.primal_values_ << std::endl;
 	std::shared_ptr<matrix_base_class> jac_ci = nlp_->constraints_inequality_jacobian(iterate);
-    if (PRINT) std::cout << "jac_ci: " << jac_ci << std::endl;
-    // if (PRINT) std::cout << "step: " << step.primal_values_ << std::endl;
-    std::vector<double> con_values_ieq_mult_result = jac_ci->multiply(step.primal_values_);
+    std::vector<double> con_values_ieq_mult_result = jac_ci->multiply(step.get_primal_values());
     for (size_t row=0; row < nlp_->num_dual_ieq(); ++row) {
         con_values_ieq[row] += con_values_ieq_mult_result[row];
     }
@@ -47,9 +48,12 @@ double ConstraintViolationFunction::operator()(const iSQOIterate &iterate, const
 }
 
 double ConstraintViolationFunction::two_vectors(const std::vector<double> &eq_items, const std::vector<double> &ieq_items) const {
+    bool PRINT = false;
 	double eq_violation = 0.0;
 	for (size_t dual_eq_index=0; dual_eq_index<eq_items.size(); ++dual_eq_index) {
-		eq_violation += abs(eq_items[dual_eq_index]);
+        if (PRINT) std::cout << "eq_violation pre step dual_eq_index=" << dual_eq_index << ": " << eq_violation ;
+		eq_violation += fabs(eq_items[dual_eq_index]);
+        if (PRINT) std::cout << "; eq_violation @ step dual_eq_index=" << dual_eq_index << ": " << eq_violation << std::endl;
 	}
 	double ieq_violation = 0.0;
 	for (size_t dual_ieq_index=0; dual_ieq_index<ieq_items.size(); ++dual_ieq_index) {
@@ -57,5 +61,8 @@ double ConstraintViolationFunction::two_vectors(const std::vector<double> &eq_it
 		ieq_violation += bracket_plus(ieq_items[dual_ieq_index]);
 		// }
 	}
+    if (PRINT) std::cout << "constraintviolationfunction  eq_violation: " << eq_violation << std::endl;
+	if (PRINT) std::cout << "constraintviolationfunction ieq_violation: " << ieq_violation << std::endl;
+    
 	return eq_violation + ieq_violation;
 }

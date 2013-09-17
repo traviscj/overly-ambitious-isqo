f519e81623740541558a932304f0b8a99a20a2c7
traviscj (mbair) <traviscj@traviscj.com>
2013-09-16 22:19:45 -0500

    linear model reduction freaks out if LMR < 0

--- src/linear_model_reduction.cc
+++ src/linear_model_reduction.cc
@@ -12,26 +12,27 @@ double LinearReductionFunction::operator()(const iSQOIterate &iterate, const iSQ
     double iterate_constraint_violation = constraint_violation_func_(iterate);
     double iterate_step_constraint_violation = constraint_violation_func_(iterate,step);
     double retval =  penalty_scaled_objective_decrease + iterate_constraint_violation - iterate_step_constraint_violation;
-    if (PRINT && (retval < 0)) {
+    if ((retval < 0)) {
     	std::cout << std::endl;
         std::cout << "linear decrease was negative!!" << std::endl;
         std::cout << "iterate: " << iterate << std::endl;
+        std::cout << "step: " << step << std::endl;
+        std::cout << "gradient: " << gradient << std::endl;
         std::cout << "nlp eq: " << nlp_->constraints_equality(iterate) << std::endl;
         std::cout << "nlp ieq: " << nlp_->constraints_inequality(iterate) << std::endl;
         
         LinearModelFunction lmf(*nlp_);
         iSQOStep zero_step(nlp_->num_primal(), nlp_->num_dual_eq(), nlp_->num_dual_ieq(), -13, -13);
-    	std::cout << " - : " << lmf(iterate, zero_step) << std::endl;
-    	std::cout << " -      : " << lmf(iterate, step) << std::endl;
-        std::cout << " -      : " << lmf(iterate, zero_step)-lmf(iterate, step) << std::endl;
+    	std::cout << " - linear model of zero step: " << lmf(iterate, zero_step) << std::endl;
+    	std::cout << " - linear model of real step: " << lmf(iterate, step) << std::endl;
+        std::cout << " - linear decrease          : " << lmf(iterate, zero_step)-lmf(iterate, step) << std::endl;
     	
-        
     	std::cout << " - penalty_scaled_objective_decrease: " << penalty_scaled_objective_decrease << std::endl;
     	std::cout << " - iterate_constraint_violation     : " << iterate_constraint_violation << std::endl;
     	std::cout << " - iterate_step_constraint_violation: " << iterate_step_constraint_violation << std::endl;
     	std::cout << " - retval                           : " << retval << std::endl;
         
-        printf("psod: %9.2e\ticv: %9.2e\tiscv: %9.2e\t\t\tlinear reduction%9.2e\n", penalty_scaled_objective_decrease, iterate_constraint_violation, iterate_step_constraint_violation, retval);
+        printf("psod: %9.2e\ticv: %9.2e\tiscv: %9.2e\t\t\tlinear reduction: %9.2e\n", penalty_scaled_objective_decrease, iterate_constraint_violation, iterate_step_constraint_violation, retval);
     }
 	return retval;
 }
